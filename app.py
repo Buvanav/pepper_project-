0cb386ad7a25d3617f376cfe1f817a75df213a7b
buvanav <buvanav224@gmail.com>
2026-01-05 00:14:29 +0530

    Fix streamlit deployment

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import tensorflow as tf
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from keras.preprocessing import image
+
 import numpy as np
 
 # Load your model
